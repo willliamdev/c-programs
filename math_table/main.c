b52242eee69a75b36d0a718c9bacb6e19b793daf
William Almeida <willliam.dev@gmail.com>
2023-08-25 20:05:51 -0300

    exercise resolution

--- math_table/main.c
+++ math_table/main.c
@@ -2,21 +2,35 @@
 
 int main()
 {
+  int number, i;
+  float divisibleNumber;
 
-  int number;
   printf("Deseja consultar a taubuada de qual número? ");
   scanf("%d", &number);
 
-  int i = 1;
-  printf("Tabuata de Soma e Subtração");
+  i = 0;
+  printf("\nSoma\t\t\tSubtração\n");
   while (i <= 10)
   {
-    printf("\n%d + %d = %d            %d + %d = %d", number, i, (number + i), number, i, (number - 1));
-
-    // condicionais pra alinhamento
+    printf("\n%d + %d = %d\t\t%d + %d = %d", number, i, (number + i), number, i, (number - i));
     i++;
   }
 
+  i = 0;
+  printf("\n\n\nMultiplicaçao\t\t\tDivisao\n");
+  divisibleNumber = number;
+  while (i <= 10)
+  {
+    if (i != 0)
+    {
+      printf("\n%d * %d = %d\t\t%d + %d = %.2f", number, i, (number * i), number, i, (divisibleNumber / i));
+    }
+    else
+    {
+      printf("\n%d * %d = %d\t\t%d + %d = Nao existe divisao por Zero", number, i, (number * i), number, i);
+    };
+    i++;
+  }
   printf("\n");
   return 0;
 }
