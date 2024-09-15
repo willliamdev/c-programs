8bf01dd660e1b079caac54b5bd6a966d8acbbcab
William Almeida <willliam.dev@gmail.com>
2024-09-15 12:24:10 -0300

    fix: use numbers instead of numbers to comparsion

--- operators/ex005.c
+++ operators/ex005.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
+#include <locale.h>
 
-// Calcule o ano de nascimento de uma pessoa a partir de sua idade e do ano atual.
 int main()
 {
-  int age;
-  char not_turn;
+  int age, not_turn;
+
   printf("How old are you? ");
   scanf("%d", &age);
-  printf("Will you turn %d this year? [y/n]: ", age + 1);
 
-  /*
-   blank in the format string tells scanf to skip leading whitespace, and the first
-   non-whitespace character will be read with the %c conversion specifier.
-  */
-  scanf(" %c", &not_turn);
+  printf("Will you turn %d this year?", age + 1);
+  printf("\n 0 - NO\n 1 - YES\nAwnser[0/1]: ");
+  scanf("%d", &not_turn);
 
-  if ((not_turn == "Y") || (not_turn == "y"))
+  if (not_turn)
   {
-    printf("oh, You was born on %d\n", (2024 - age + 1));
+    printf("\nYou was born on %d\n", (2024 - (age + 1)));
   }
   else
   {
